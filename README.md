d8d3bb9ac74286cb9fcb5d66c6673c990355dc4f
Tazri <mdtazri@gmail.com>
2024-02-13 23:57:38 +0600

    complete

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# Module 4 : 4.26 - Reusing Logic with Custom Hooks - Part 4
+# Module 4 : 4.27 - Reusing Logic with Custom Hooks - Part 5
 
-This is another reason for why wrapping Effects in custom Hooks is often beneficial:
+## ☕ Recap
 
-- You make the data flow to and from your Effects very explicit.
-- You let your components focus on the intent rather than on the exact implementation of your Effects.
-- When React adds new features, you can remove those Effects without changing any of your components.
+- Custom Hooks let you share logic between components.
+- Custom Hooks must be named starting with use followed by a capital letter.
+- Custom Hooks only share stateful logic, not state itself.
+- You can pass reactive values from one Hook to another, and they stay up-to-date.
+- All Hooks re-run every time your component re-renders.
+  The code of your custom Hooks should be pure, like your component’s code.
+- Wrap event handlers received by custom Hooks into Effect Events.
+- Don’t create custom Hooks like useMount. Keep their purpose specific.
+- It’s up to you how and where to choose the boundaries of your code.
--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,21 @@
-import {
-  useEffect,
-  experimental_useEffectEvent as useEffectEvent,
-  useState,
-} from "react";
+import { useState } from "react";
+import Welcome from "./Welcome";
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  const [increament, setIncreament] = useState(1);
-
-  const onTick = useEffectEvent(() => {
-    setCount((c) => c + increament);
-  });
-
-  // useEffect(() => {
-  //   const timerId = setInterval(() => {
-  //     setCount((c) => c + increament);
-  //   }, 1000);
-
-  //   console.log("> Increament");
-
-  //   return () => clearInterval(timerId);
-  // }, [increament]);
-
-  useEffect(() => {
-    const timerId = setInterval(onTick, 1000);
-
-    console.log("runnen");
-    return () => clearInterval(timerId);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const [show, setShow] = useState(false);
 
   return (
     <>
-      <div className="w-1/2 mx-auto p-3 text-left flex flex-col gap-y-1">
-        <div className="flex gap-x-6 items-center">
-          <p className="text-xl">Counter : {count}</p>
-          <button onClick={() => setCount(0)} className="button button-failed">
-            Reset
-          </button>
-          <button
-            className="button button-success"
-            onClick={() => console.clear()}
-          >
-            Clear Console
-          </button>
-        </div>
+      <div className="w-1/2 mx-auto p-3 text-left flex flex-col gap-y-1 items-start">
+        <button
+          className="button button-success"
+          onClick={() => setShow((s) => !s)}
+        >
+          {show ? "Hide" : "Show"}
+        </button>
+        <hr className="text-white w-full my-3" />
 
-        <hr className="my-3" />
-        <div className="flex flex-row flex-wrap gap-x-4 items-center">
-          <button
-            disabled={increament == 0}
-            onClick={() => setIncreament((i) => i - 1)}
-            className="button-circle_mini button-failed text-lg"
-          >
-            -
-          </button>
-          <span className="text-lg">{increament}</span>
-          <button
-            onClick={() => setIncreament((i) => i + 1)}
-            className="button-circle_mini button-success text-lg"
-          >
-            +
-          </button>
-        </div>
+        {show && <Welcome />}
       </div>
     </>
   );
