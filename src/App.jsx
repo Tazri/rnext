4674f51e646152b7b5539722c052fbf67028326d
Tazri <mdtazri@gmail.com>
2024-03-24 14:54:51 +0600

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import ProductList from "./components/ProductList";
+import Tutorial from "./components/Tutorial";
+import GlobalStyle from "./components/styles/GlobalStyle.style";
 
 export default function App() {
   return (
-    <div className="bg-slate-950 text-white min-w-screen min-h-screen">
-      <h1 className="text-3xl text-center py-4">React Infinite Scroll</h1>
-
-      <ProductList />
-    </div>
+    <>
+      <GlobalStyle />
+      <Tutorial />
+    </>
   );
 }
