d3c0431a8e98945a336dcbc2626c707c49e8f6c1
Tazri <mdtazri@gmail.com>
2024-07-09 04:24:52 +0600

    complete part 7

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,50 @@
-import { Link } from "react-router-dom";
-import useAuth from "../hooks/useAuth";
+import { useEffect, useReducer } from "react";
+import { actions } from "../action/index.js";
+import { initialState, postReducer } from "../reducers/postReducer";
+import PostList from "./../components/posts/PostList";
+import { useAxios } from "./../hooks/useAxios.js";
 
 export default function HomePage() {
-  const { auth } = useAuth();
+  const [state, dispatch] = useReducer(postReducer, initialState);
+  const api = useAxios();
 
-  console.log("> HomePage.jsx -> auth");
-  console.log(auth);
+  useEffect(() => {
+    dispatch({ type: actions.post.DATA_FETCHING });
+
+    async function fetchPost() {
+      try {
+        const response = await api.get(
+          `${import.meta.env.VITE_SERVER_BASE_URL}/posts`
+        );
+
+        if (response.status === 200) {
+          dispatch({ type: actions.post.DATA_FETCHED, data: response.data });
+        }
+      } catch (err) {
+        dispatch({ type: actions.post.DATA_FETCH_ERROR, error: err.message });
+      }
+    }
+
+    fetchPost();
+  }, []);
+  if (state?.loading) {
+    return <div>Data is loading....</div>;
+  }
+
+  if (state?.error) {
+    return (
+      <div>
+        <p>There is something wrong in fetching post...</p>
+        <span>{state?.error?.message}</span>
+      </div>
+    );
+  }
+
   return (
     <>
-      <h1>HomePage</h1>
-      <Link to="/me">Profile</Link>
+      <div>
+        <PostList posts={state?.posts} />
+      </div>
     </>
   );
 }
